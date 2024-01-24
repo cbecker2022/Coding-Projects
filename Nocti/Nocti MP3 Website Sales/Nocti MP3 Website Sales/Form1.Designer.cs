0d7fef186f0c1c958aa8d97e1929df68c423365f
cbecker2022 <117120242+cbecker2022@users.noreply.github.com>
2024-01-24 13:47:12 -0500

    Upload Nocti Project

---
+++ Nocti/Nocti MP3 Website Sales/Nocti MP3 Website Sales/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿
+namespace Nocti_MP3_Website_Sales
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.cboItem = new System.Windows.Forms.ComboBox();
+            this.cboQuantity = new System.Windows.Forms.ComboBox();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.lboxOutput = new System.Windows.Forms.ListBox();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblCumlItem = new System.Windows.Forms.Label();
+            this.lblCumlProf = new System.Windows.Forms.Label();
+            this.printCapture = new Microsoft.VisualBasic.PowerPacks.Printing.PrintForm(this.components);
+            this.SuspendLayout();
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(180, 206);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(61, 23);
+            this.btnCalc.TabIndex = 4;
+            this.btnCalc.Text = "&Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(444, 206);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(69, 23);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Item:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Quantity:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(15, 28);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(159, 20);
+            this.txtName.TabIndex = 0;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // cboItem
+            // 
+            this.cboItem.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboItem.FormattingEnabled = true;
+            this.cboItem.Items.AddRange(new object[] {
+            "Lucy Nelson\'s Greatest Hits",
+            "Barry Cuda & The Sharks LIVE",
+            "Busta Move Boogies"});
+            this.cboItem.Location = new System.Drawing.Point(15, 116);
+            this.cboItem.Name = "cboItem";
+            this.cboItem.Size = new System.Drawing.Size(159, 21);
+            this.cboItem.TabIndex = 2;
+            this.cboItem.SelectedIndexChanged += new System.EventHandler(this.cboItem_SelectedIndexChanged);
+            // 
+            // cboQuantity
+            // 
+            this.cboQuantity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboQuantity.FormattingEnabled = true;
+            this.cboQuantity.Location = new System.Drawing.Point(15, 206);
+            this.cboQuantity.Name = "cboQuantity";
+            this.cboQuantity.Size = new System.Drawing.Size(159, 21);
+            this.cboQuantity.TabIndex = 3;
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Enabled = false;
+            this.dtpDate.Location = new System.Drawing.Point(180, 28);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(329, 20);
+            this.dtpDate.TabIndex = 1;
+            // 
+            // lboxOutput
+            // 
+            this.lboxOutput.FormattingEnabled = true;
+            this.lboxOutput.HorizontalScrollbar = true;
+            this.lboxOutput.Location = new System.Drawing.Point(180, 53);
+            this.lboxOutput.Name = "lboxOutput";
+            this.lboxOutput.Size = new System.Drawing.Size(233, 147);
+            this.lboxOutput.TabIndex = 9;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(360, 206);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(64, 23);
+            this.btnPrint.TabIndex = 5;
+            this.btnPrint.Text = "&Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(268, 206);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(68, 23);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "C&lear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(419, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Cumulative Items:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(419, 141);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Cumulative Profits:";
+            // 
+            // lblCumlItem
+            // 
+            this.lblCumlItem.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCumlItem.Location = new System.Drawing.Point(422, 87);
+            this.lblCumlItem.Name = "lblCumlItem";
+            this.lblCumlItem.Size = new System.Drawing.Size(91, 26);
+            this.lblCumlItem.TabIndex = 13;
+            // 
+            // lblCumlProf
+            // 
+            this.lblCumlProf.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCumlProf.Location = new System.Drawing.Point(422, 157);
+            this.lblCumlProf.Name = "lblCumlProf";
+            this.lblCumlProf.Size = new System.Drawing.Size(91, 26);
+            this.lblCumlProf.TabIndex = 14;
+            // 
+            // printCapture
+            // 
+            this.printCapture.DocumentName = "document";
+            this.printCapture.Form = this;
+            this.printCapture.PrintAction = System.Drawing.Printing.PrintAction.PrintToPreview;
+            this.printCapture.PrinterSettings = ((System.Drawing.Printing.PrinterSettings)(resources.GetObject("printCapture.PrinterSettings")));
+            this.printCapture.PrintFileName = null;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 240);
+            this.Controls.Add(this.lblCumlProf);
+            this.Controls.Add(this.lblCumlItem);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.lboxOutput);
+            this.Controls.Add(this.dtpDate);
+            this.Controls.Add(this.cboQuantity);
+            this.Controls.Add(this.cboItem);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalc);
+            this.Name = "frmMain";
+            this.Text = "Main Form";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.ComboBox cboItem;
+        private System.Windows.Forms.ComboBox cboQuantity;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.ListBox lboxOutput;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblCumlItem;
+        private System.Windows.Forms.Label lblCumlProf;
+        private Microsoft.VisualBasic.PowerPacks.Printing.PrintForm printCapture;
+    }
+}
+
---
+++ Nocti/Nocti MP3 Website Sales/Nocti MP3 Website Sales/Form1.cs
@@ -0,0 +1,174 @@
+﻿/* Cody Becker
+ * April 20th, 2022
+ * Program to track orders for MP3 Albums on a website*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nocti_MP3_Website_Sales
+{
+    public partial class frmMain : Form
+    {
+        // initialize constant variables
+        const double SALES_TAX = 0.06;
+        // initialize array variables
+        int[] aryQuantities = { 0, 0, 0 };
+        double[] aryListPrice = {13.99, 37.99, 41.99};
+        double[] aryProfit = {4.00, 11.00, 12.00};
+        // initialize integer variables
+        int LucyQuantity = 0, BarryQuantity = 0, BustaQuantity = 0, cumlItems = 0;
+        // initialize double variables
+        double LucyTotal = 0.00, BarryTotal = 0.00, BustaTotal = 0.00, LucyProf = 0.00, BarryProf = 0.00, BustaProf = 0.00, subtotal = 0.00, salesTax = 0.00, shippingFees = 0.00, orderTotal = 0.00, cumlProfit = 0.00;
+        // initialize string variables
+        string CustomerName = "", Date = "", TempName = "";
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            // for loop to add numbers to the quantity combobox up to 100
+            for (int x = 1; x <= 100; ++x)
+            {
+                cboQuantity.Items.Add(x);
+            }
+            // sets defaults of the comboboxes
+            cboItem.SelectedIndex = 0;
+            cboQuantity.SelectedIndex = 0;
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // closes the program
+            this.Close();
+        }
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            // switch case statement to find the quantity of an item the customer orders
+            // it will then determine the profit and totals for that specific item
+            switch (cboItem.SelectedIndex)
+            {
+                case 0:
+                    LucyQuantity = cboQuantity.SelectedIndex + 1;
+                    aryQuantities[0] += LucyQuantity;
+                    LucyProf += LucyQuantity * aryProfit[0];
+                    LucyTotal += LucyQuantity * aryListPrice[0];
+                    break;
+                case 1:
+                    BarryQuantity = cboQuantity.SelectedIndex + 1;
+                    aryQuantities[1] += BarryQuantity;
+                    BarryProf += BarryQuantity * aryProfit[1];
+                    BarryTotal += BarryQuantity * aryListPrice[1];
+                    break;
+                case 2:
+                    BustaQuantity = cboQuantity.SelectedIndex + 1;
+                    aryQuantities[2] += BustaQuantity;
+                    BustaProf += BustaQuantity * aryProfit[2];
+                    BustaTotal += BustaQuantity * aryListPrice[2];
+                    break;
+            }
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // clears the listbox
+            lboxOutput.Items.Clear();
+            // sets customer name variables to nothing
+            CustomerName = "";
+            TempName = "";
+            // sets the lucy album information to zero
+            LucyProf = 0;
+            LucyQuantity = 0;
+            LucyTotal = 0;
+            // sets the barry album information to zero
+            BarryProf = 0;
+            BarryQuantity = 0;
+            BarryTotal = 0;
+            // sets the busta album information to zero
+            BustaProf = 0;
+            BustaQuantity = 0;
+            BustaTotal = 0;
+            // sets the calculated information to zero
+            subtotal = 0;
+            salesTax = 0;
+            shippingFees = 0;
+            orderTotal = 0;
+            // for loop set all array quantities values to zero
+            for (int x = 0; x < 3; ++x)
+            {
+                aryQuantities[x] = 0;
+            }
+            // sets the customer name textbox back to nothing
+            txtName.Text = "";
+            // sets the combobox back to the default info
+            cboItem.SelectedIndex = 0;
+            cboQuantity.SelectedIndex = 0;
+        }
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            // gets customer name and date assigned to variables
+            CustomerName = txtName.Text;
+            TempName = CustomerName.ToLower();
+            Date = dtpDate.Value.ToString();
+            // rounds the totals each album
+            LucyTotal = Math.Round(LucyTotal, 2);
+            BarryTotal = Math.Round(BarryTotal, 2);
+            BustaTotal = Math.Round(BustaTotal, 2);
+            // adds item order information to the listbox
+            lboxOutput.Items.Add(string.Format("Lucy Nelson's Greatest Hits {0} for {1}", aryQuantities[0], LucyTotal.ToString("C2")));
+            lboxOutput.Items.Add(string.Format("Barry Cuda & The Sharks LIVE {0} for {1}", aryQuantities[1], BarryTotal.ToString("C2")));
+            lboxOutput.Items.Add(string.Format("Busta Move Boogies {0} for {1}", aryQuantities[2], BustaTotal.ToString("C2")));
+            // calculates subtotal and rounds it
+            subtotal = LucyTotal + BarryTotal + BustaTotal;
+            // calculates sales tax and rounds it
+            salesTax = subtotal * SALES_TAX;
+            salesTax = Math.Round(salesTax, 2);
+            // switch case statement to determine shipping fees based on temporary name
+            switch (TempName)
+            {
+                case "susan smith":
+                    shippingFees = 10.00;
+                    break;
+                case "george jackson":
+                    shippingFees = 9.68;
+                    break;
+            }
+            // calculates the order total
+            orderTotal = subtotal + salesTax + shippingFees;
+            // calculates cumulative information and displays it
+            cumlItems += aryQuantities[0] + aryQuantities[1] + aryQuantities[2];
+            cumlProfit += LucyProf + BarryProf + BustaProf;
+            lblCumlItem.Text = cumlItems.ToString();
+            lblCumlProf.Text = cumlProfit.ToString("C2");
+            // adds calculated information to the listbox
+            lboxOutput.Items.Add(string.Format("Name: {0}", CustomerName));
+            lboxOutput.Items.Add(string.Format("Date: {0}", Date));
+            lboxOutput.Items.Add(string.Format("Subtotal: {0}", subtotal.ToString("C2")));
+            lboxOutput.Items.Add(string.Format("Sales Tax: {0}", salesTax.ToString("C2")));
+            lboxOutput.Items.Add(string.Format("Shipping Fees: {0}", shippingFees.ToString("C2")));
+            lboxOutput.Items.Add(string.Format("Order Total: {0}", orderTotal.ToString("C2")));
+            // message box displaying all information combined
+            MessageBox.Show(string.Format("Name: {0}\nDate: {1}\nLucy Nelson's Greatest Hits {2} for {3}\nBarry Cuda & The Sharks LIVE {4} for {5}\nBusta Move Boogies {6} for {7}\nSubtotal: " +
+                "{8}\nSales Tax: {9}\nShipping Fees: {10}\nOrder Total: {11}", CustomerName, Date, aryQuantities[0], LucyTotal.ToString("C2"), aryQuantities[1], BarryTotal.ToString("C2"), 
+                aryQuantities[2], BustaTotal.ToString("C2"), subtotal.ToString("C2"), salesTax.ToString("C2"), shippingFees.ToString("C2"), orderTotal.ToString("C2")), "Receipt", 
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            printCapture.Print();
+            btnClear.PerformClick();
+        }
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+            // set combobox quantity and item to default index if name changes
+            cboItem.SelectedIndex = 0;
+            cboQuantity.SelectedIndex = 0;
+        }
+        private void cboItem_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // set combobox quantity to default index if item changes
+            cboQuantity.SelectedIndex = 0;
+        }
+    }
+}
